a07cd834bff5d0757dcb3f441bdb35afb2855779
Jaymar Catapang <jaymarcatapang@digits.ph>
2024-06-24 07:48:26 +0800

    added supervisor columns to prf header table

--- database/migrations/2024_06_24_074709_add_ap_supervisor_columns_to_prf_header_table.php
+++ database/migrations/2024_06_24_074709_add_ap_supervisor_columns_to_prf_header_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddApCheckerCommentsToPrfHeaderTable extends Migration
+class AddApSupervisorColumnsToPrfHeaderTable extends Migration
 {
     /**
      * Run the migrations.
